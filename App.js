f2635c1d453430e1ab01f57e611e8bbd849fd47f
William Thompson <wpet01@live.se>
2023-12-11 15:16:18 +0100

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Detta är snart en app för min stora snygga starka bror som jag inte alls är bättre än mig på golf</Text>
       <StatusBar style="auto" />
     </View>
   );
